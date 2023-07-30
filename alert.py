f168d583b397fd154f265e8062b0dd378e972a11
Starwhooper <thiemo@schuff.eu>
2023-07-30 17:17:56 +0200

    Update alert.py
    add amination support

--- alert.py
+++ alert.py
@@ -9,17 +9,14 @@
 #######################################################
 
 # -*- coding:utf-8 -*-
-#from PIL import Image,ImageDraw,ImageFont,ImageColor
-#from datetime import datetime
-#import RPi.GPIO as GPIO
+from ffmpeg import FFmpeg, Progress
 import argparse
+import glob
 import json
 import os
 import requests
-#import subprocess
 import sys
 import time
-#import urllib.request
 
 scriptfolder = os.path.split(os.path.abspath(__file__))[0]
 
@@ -36,6 +33,11 @@ parser = argparse.ArgumentParser(
     prog='RPI-motionalery by Starwhooper',
     description="send pushover messager for motion",
     epilog='keep watching https://github.com/Starwhooper/RPi-motionalert')
+
+parser.add_argument(
+    "-a", "--animation", 
+    action='store_true', 
+    help="convert video to gif animation (makes only sense as event after motion has ended")
 
 parser.add_argument(
     "-c", "--cameraid", 
@@ -71,13 +73,36 @@ args = parser.parse_args()
 if (os.path.isdir(cf['folder'] + "/Camera" + str(args.cameraid))) == False : sys.exit('exit: folder ' + cf['folder'] + '/Camera' + str(args.cameraid) + ' not found, please check camera ID')
 
 if (len(args.picture) >= 1):
-    if (os.path.isfile(cf['folder'] + "/Camera" + str(args.cameraid) + '/' + args.picture)) == False : sys.exit('exit: ' + cf['folder'] + "/Camera" + str(args.cameraid) + '/' + args.picture + ' not found')
-    time.sleep(2)
-    output_picturepath = cf['folder'] + "/Camera" + str(args.cameraid) + '/' + args.picture
+    if (args.animation == False):
+        if (os.path.isfile(cf['folder'] + "/Camera" + str(args.cameraid) + '/' + args.picture)) == False : sys.exit('exit: ' + cf['folder'] + "/Camera" + str(args.cameraid) + '/' + args.picture + ' not found')
+        time.sleep(2)
+        output_picturepath = cf['folder'] + "/Camera" + str(args.cameraid) + '/' + args.picture
+    else:
+        newestdate = newestfile = 0
+        for file in glob.glob(cf['folder'] + "/Camera" + str(args.cameraid) + '/' + args.picture, recursive=False):
+            if newestdate <= os.path.getmtime(file):
+                newestdate = os.path.getmtime(file)
+                newestfile = file
+        
+        ffmpeg = (
+            FFmpeg()
+            .option("y")
+            .input(newestfile)
+            .output(
+                newestfile+".gif",
+                vf="scale=320:-1:flags=lanczos"
+            )
+        )
+
+        ffmpeg.execute()
+        output_picturepath = newestfile+".gif"
+        
 else: output_picturepath = scriptfolder + "/example.gif"
 
 if (args.messagetitle == ''): output_messagetitle = "Camera " + str(args.cameraid)
 else: output_messagetitle = args.messagetitle
+
+if (args.animation == True): output_messagetitle = output_messagetitle + " (GIF)"
 
 output_messagebody = args.messagebody
 
